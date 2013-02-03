30edb604c8cc59ad3ddb4537b9251a716106422d
takutok <tkhearts@gmail.com>
2013-02-03 14:48:43 +0900

    commit

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ CONFIG = {};
 CONFIG.GITHUB_API = "https://api.github.com/";
 
 
-CONFIG.DEFAULT_USER = "takutok";
-CONFIG.DEFAULT_PASS = "gagagatetete77";
+//CONFIG.DEFAULT_USER = "takutok";
 
 //CONFIG.REPOS_NAME = "WIMM";
 //CONFIG.REPOS_PARENT = "qiita-github-api-hackathon";
@@ -11,7 +10,8 @@ CONFIG.DEFAULT_PASS = "gagagatetete77";
 
 
 CONFIG.REPOS_PARENT = "PGMY";
-CONFIG.REPOS_NAME = "QiitaHackathonTest";
+//CONFIG.REPOS_NAME = "QiitaHackathonTest";
+CONFIG.REPOS_NAME = "MakeHerMine";
 
 CONFIG.Q_DATA_DIRECTORY="data";
 CONFIG.Q_FILE_PATH="question.json";
